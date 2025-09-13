5d391d6c6ea95c006d3d736cb0828d7874bfcc1d
GitHub <csy5271abcd@gmail.com>
2025-09-13 15:52:20 +0900

    페이지 SEO 작업

--- src/components/section/Main.jsx
+++ src/components/section/Main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
-const Main = () => {
+import Header from './Header'
+import Footer from './Footer'
+
+const Main = ( props ) => {
+    // props를 통해 -> data 전달
     return (
-        <main id="main" role="main">
-            main  
-        </main>
+        <HelmetProvider>
+            <Helmet 
+                titleTemplate="%s | Webs Youtube" 
+                defaultTitle="Webs Youtube" 
+                defer={false}
+            >
+                {props.title && <title>{props.title}</title>}
+                <meta name="description" content={props.description} />
+            </Helmet>
+
+            <Header />
+            <main id="main" role="main">
+                {props.children}
+            </main>
+            <Footer />
+        </HelmetProvider>
     )
 }
 
