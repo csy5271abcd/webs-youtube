4f9ad78acac77eda36a09bab5f6ab709e7c39cfa
GitHub <csy5271abcd@gmail.com>
2025-09-16 16:59:38 +0900

    Swiper 이미지 슬라이드 작업

--- src/components/contents/Developer.jsx
+++ src/components/contents/Developer.jsx
@@ -1,27 +1,84 @@
-import React from 'react'
-
-import { developerText } from '../../data/developer';
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 
-const Developer = () => {
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Autoplay } from 'swiper/modules';
+
+const Developer = ({ videos, title, id }) => {
+    const [loading, setLoading] = useState(true); 
+
+    useEffect(() => {
+        // 0.5초 후 -> setLoading false
+        setTimeout(() => {
+            setLoading(false);
+        }, 500);
+    }, []);
+
+    const developerClass = loading ? 'isLoading' : 'isLoaded';
+
     return (
-        <section id='developer'>
-            <h2>😪 추천 개발자를 소개합니다.</h2>
-            <div className="developer__inner overflow">
-                {developerText.map((developer, key) => (
-                    <div className="developer" key={key}>
-                        <div className="developer__img play__icon">
-                            <Link to={`/channel/${developer.channelId}`}>
-                                <img src={developer.img} alt={developer.name} />
-                            </Link>
-                        </div>
-                        <div className="developer__info">
-                            <Link to={`/channel/${developer.channelId}`}>
-                                {developer.name}
-                            </Link>
-                        </div>
-                    </div>
-                ))}
+        <section id={id} className={developerClass}>
+            <h2>{title}</h2>
+
+            <div className='developer__inner'>
+                <Swiper
+                    slidesPerView={4}
+                    spaceBetween={15}
+                    navigation={true}
+                    autoplay={{
+                        delay: 5000,
+                        disableOnInteraction: false,
+                    }}
+                    breakpoints={{
+                        640: {
+                            slidesPerView: 5,
+                            spaceBetween: 25,
+                        },
+                        768: {
+                            slidesPerView: 6,
+                            spaceBetween: 25,
+                        },
+                        1024: {
+                            slidesPerView: 7,
+                            spaceBetween: 30,
+                        },
+                        1240: {
+                            slidesPerView: 8,
+                            spaceBetween: 30,
+                        },
+                        1640: {
+                            slidesPerView: 9,
+                            spaceBetween: 30,
+                        },
+                        2000: {
+                            slidesPerView: 10,
+                            spaceBetween: 30,
+                        },
+                    }}
+                    modules={[Navigation, Autoplay]}
+                    className='mySwiper'
+                >
+                    {videos.map((developer, key) => (
+                        <SwiperSlide key={key}>
+                            <div className='developer' key={key}>
+                                <div className='developer__img play__icon'>
+                                    <Link to={`/channel/${developer.channelId}`}>
+                                        <img src={developer.img} alt={developer.name} />
+                                    </Link>
+                                </div>
+                                <div className='developer__info'>
+                                    <Link to={`/channel/${developer.channelId}`}>
+                                        {developer.name}
+                                    </Link>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
             </div>
         </section>
     )
